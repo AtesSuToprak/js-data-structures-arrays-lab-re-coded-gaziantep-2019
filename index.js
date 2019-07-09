d31faf42b9a7ba7219300afda5292c86c754d19a
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-09 09:44:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(DAD) {
+  drivers.push("Ralph");
+ 
+}
+
+function destructivelyPrependDriver(DPD) {
+  drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver(DRLD) {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver (DRFD) {
+  drivers.shift();
+}
+
+function appendDriver (AD) {
+  
+  
+}
+
+
+
+/*  describe('appendDriver(name)', function () {
+      it('appends a driver to the drivers array and returns a new array, leaving the drivers array unchanged', function () {
+        expect(appendDriver("Broom")).to.have.ordered.members(["Milo", "Otis", "Garfield", "Broom"]);
+
+        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+
+    describe('prependDriver(name)', function () {
+      it('prepends a driver to the drivers array and returns a new array, leaving the drivers array unchanged', function () {
+        expect(prependDriver("Arnold")).to.have.ordered.members(["Arnold", "Milo", "Otis", "Garfield"]);
+
+        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+
+    describe('removeLastDriver()', function () {
+      it('removes the last driver in the drivers array and returns a new array, leaving the drivers array unchanged', function () {
+        expect(removeLastDriver()).to.have.ordered.members(["Milo", "Otis"]);
+
+        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+
+    describe('removeFirstDriver()', function () {
+      it('removes the first driver from the drivers array and returns a new array, leaving the drivers array unchanged', function () {
+        expect(removeFirstDriver()).to.have.ordered.members(["Otis", "Garfield"]);
+
+        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);*/
